6a551c5021a3b441adc721d9ebf28e18893d9912
Simon O'Donnell <simonrodonnell@yahoo.co.uk>
2019-12-09 15:32:40 +0000

    MVP done

--- src/components/FilmItem.js
+++ src/components/FilmItem.js
@@ -5,7 +5,8 @@ class FilmItem extends Component {
   render(){
     return(
       <li className="FilmItem">
-        <a href={this.props.url}>{this.props.children}</a>
+        <a href={this.props.url}>
+        {this.props.children}</a>
       </li>
     )
   }
